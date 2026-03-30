02e49b8bd4cb75f3354b63e2175a96b8e5d687ad
Hasiba Dolovac <162062697+HasibaDolovac@users.noreply.github.com>
2026-03-30 03:10:51 +0200

    update

--- routes/resources/views/layouts/okvir.blade.php
+++ routes/resources/views/layouts/okvir.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AutoEdu - Sistem za upravljanje</title>
-    <link rel="stylesheet" href="{{ asset('css/stil.css') }}">
+   <link rel="stylesheet" href="{{ asset('css/stil.css') }}">
     <style>
        
         .glavna-navigacija {
